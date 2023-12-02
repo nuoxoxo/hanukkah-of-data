324f0790608b2760ffb5383a372b3280631a39bb
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-12-02 19:05:57 +0100

    2879 : 3rd way - using int-location

--- pd_2879_df_head_5_by_default.py
+++ pd_2879_df_head_5_by_default.py
@@ -1,10 +1,14 @@
 import pandas as pd
 
 def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
+
+    # Way 3 : int-location
+    return employees.iloc[0:3]
+
     # Way 2 : treat DF as list
     return employees[:3]
+
     # Way 1 : head method
-    """
     # print(type(employees.head), '<--- will be <class> \'method\'')
     return employees.head(3)
-    """
+
