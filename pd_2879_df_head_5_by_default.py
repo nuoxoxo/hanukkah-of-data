59707327a36032276ff22b39b053821ab8ed2023
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-12-02 19:03:12 +0100

    2879 : 2nd way - DF seen as list

--- pd_2879_df_head_5_by_default.py
+++ pd_2879_df_head_5_by_default.py
@@ -1,5 +1,10 @@
 import pandas as pd
 
 def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
+    # Way 2 : treat DF as list
+    return employees[:3]
+    # Way 1 : head method
+    """
     # print(type(employees.head), '<--- will be <class> \'method\'')
     return employees.head(3)
+    """
