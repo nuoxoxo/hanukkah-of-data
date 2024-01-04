68ba929b20cda8861835df8095ffa4ec04ac7aea
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-01-04 18:45:16 +0100

    + 2887 - fill missing data : + iteration method

--- some-lc-problems-to-get-going/2887_fill_missing.py
+++ some-lc-problems-to-get-going/2887_fill_missing.py
@@ -1,6 +1,15 @@
 import pandas as pd
 
 def fillMissingValues(products: pd.DataFrame) -> pd.DataFrame:
+
+    # way 2 - isnull + iterrows + .at[idx,'key']
+    for idx, row in  products.iterrows():
+        if not pd.isnull( row['quantity'] ):
+            continue
+        products.at[ idx, 'quantity' ] = 0
+    return products
+
+    # way 1 - using fillna
     return products.fillna(
         value = { 'quantity': 0 }
     )
