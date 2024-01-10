e406e7f10481e0c2988327c1f71575078af5d6f3
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-01-10 23:33:09 +0100

    + 1 in python script : shortened, improved

--- 5784/1.py
+++ 5784/1.py
@@ -1,27 +1,17 @@
 import pandas as pd
-NC = pd.read_csv('noahs-customers.csv')
-print(NC)
-print(NC.head())
+df = pd.read_csv('noahs-customers.csv')
 
-phone2 = NC['phone'].str.replace('-','')
-NC['phone2'] = phone2
-phone2_idx = NC.columns.get_loc("phone2")
-phone_idx = NC.columns.get_loc("phone")
-cols = NC.columns.tolist()
-cols = cols[:phone_idx + 1] + cols[phone2_idx:phone2_idx + 1] + cols[phone_idx + 1:phone2_idx]
-NC = NC[cols]
+# step 1/3 : get rid of '-' in phone numbers
 
-fullnames = NC['name'].str.split(expand=True) # Expand split strs into separate cols
+df['phone_number'] = df['phone'].str.replace('-','')
+
+# step 2/3 : get all the last names
+
+fullnames = df['name'].str.split(expand=True) # Expand split strs into separate cols
 midl, last = fullnames[1], fullnames[2]
-last = last.mask( last.isnull(), midl )
-NC['last'] = last
-name_idx = NC.columns.get_loc("name")
-last_idx = NC.columns.get_loc("last")
-cols = NC.columns.tolist()
-cols = cols[:name_idx + 1] + cols[last_idx:last_idx + 1] + cols[name_idx + 1:last_idx]
-NC = NC[cols]
+df['last'] = last.mask( last.isnull(), midl )
 
-NC = NC[['last','phone2']]
+# step 3/3 : compare 2 cols - \phone_number and \last
 
 def last_name_to_phone_number (last):
     D={'abc':2,'def':3,'ghi':4,'jkl':5,'mno':6,'pqrs':7,'tuv':8,'wxyz':9}
@@ -33,7 +23,8 @@ def last_name_to_phone_number (last):
                 break
     return res
 
-NC['to_check'] = NC['last'].apply( last_name_to_phone_number )
+df = df[ ['last','phone_number'] ]
+df['smart_number'] = df['last'].apply( last_name_to_phone_number )
+res = df[ df['phone_number'] == df['smart_number'] ]
 
-res = NC[ NC['phone2'] == NC['to_check'] ]
 print(res,'/res')
