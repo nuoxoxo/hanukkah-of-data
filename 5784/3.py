e681dacda2fffcac2e5fef25f14853a051b2176d
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-01-10 23:43:07 +0100

    minor

---
+++ 5784/3.py
@@ -0,0 +1,23 @@
+import pandas as pd
+df = pd.read_csv('noahs-customers.csv')
+
+# filter cancer people born a rabbit
+
+def is_cancer_of_rabbit(date: str):
+    year, month, day = date.split('-')
+    if int(year) % 4 == 3 and\
+    ((month == '06' and 22<=int(day)<=30) or\
+    (month == '07' and 1<=int(day)<=22)):
+        return True
+    return False
+
+df = df[df['birthdate'].apply(lambda x: is_cancer_of_rabbit(x))]
+
+# filter 
+
+def is_jamaica( city: str):
+    return city.startswith('Jamaica') and city.split()[-1] == '11435'
+
+df = df[df['citystatezip'].apply(lambda x: is_jamaica(x))]
+
+print(df,df[['phone']], '/res')
